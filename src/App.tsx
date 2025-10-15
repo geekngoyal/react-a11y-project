7185bc8c623d9112a315dfb51a7864986740f28d
Nipun Goyal <nipungoyal96@gmail.com>
2025-10-15 17:11:27 +0530

    fix contrast

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
   const handleCalculate = () => {};
 
   return (
-    <div data-testid="app-container" style={{ padding: '20px', backgroundColor: '#fff', color: '#aaa' }}>
+    <div data-testid="app-container" style={{ padding: '20px', backgroundColor: '#fff', color: '#767676' }}>
       <img
         src='https://images.unsplash.com/photo-1594352161389-11756265d1b5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         width={600}
